37b70595bd893cdf660548e4c2c5bde39c7b8441
Petter Ahlberg <petter.ahlberg@gmail.com>
2012-11-27 13:38:57 +0100

    Added jsQueue
    Renamned jsListenerObject.htm to index.htm.

---
+++ jsQueue.js
@@ -0,0 +1,48 @@
+﻿(function (window) {
+	"use strict";
+
+	var createJsQueue,
+		util;
+
+	/* -- util -- */
+	util = {
+		addQueueItems : function (queue, queueObject) {
+			var k;
+
+			for (k in queueObject) {
+				if (queueObject.hasOwnProperty(k) && typeof queueObject[k] === "function") {
+					queue.addItem(queueObject[k]);
+				}
+			}
+		}
+	};
+	/* -- /util -- */
+
+	/* -- createJsQueue -- */
+	createJsQueue = function (queueObject) {
+		var that = window.createListenerCollection(),
+			currentMethod = 0;
+
+		util.addQueueItems(that, queueObject);
+
+		that.ready = function () {
+			currentMethod += 1;
+
+			if (!that[currentMethod]) {
+				that.trigger("ready");
+			} else {
+				that[currentMethod].apply(that, arguments);
+			}
+		};
+
+		that.exec = function () {
+			currentMethod = 0;
+			that[currentMethod].apply(that, arguments);
+		};
+
+		return that;
+	};
+	/* -- /createJsQueue -- */
+
+	window.createJsQueue = createJsQueue;
+}(window));
---
+++ jsQueue.tests.js
@@ -0,0 +1,85 @@
+﻿(function (window) {
+	"use strict";
+
+	var qUnit = window.QUnit,
+		module = qUnit.module,
+		test = qUnit.test,
+		ok = qUnit.ok,
+		strictEqual = qUnit.strictEqual;
+
+	module("jsQueue: Basic tests");
+	test("There is a global function called 'createJsQueue'", function () {
+		ok(window.createJsQueue && typeof window.createJsQueue === "function", "The global function 'createJsQueue' exists");
+	});
+
+	test("The object created by 'createJsQueue' gets an 'addItem' method and a 'length' property", function () {
+		var queue = window.createJsQueue();
+
+		ok(typeof queue.addItem === "function", "The queue has a 'addItem' method");
+		ok(typeof queue.length === "number", "The queue has a 'length' property");
+	});
+
+	test("The object created by 'createJsQueue' takes a queueObject as it's only argument", function () {
+		var queueObject = {
+				first : function () {},
+				second : function () {},
+				third : "What?",
+				fourth : function () {}
+			},
+			queue = window.createJsQueue(queueObject);
+
+		strictEqual(queue[0], queueObject.first, "The first method is added the the queue");
+		strictEqual(queue[1], queueObject.second, "The second method is added the the queue");
+		ok(queue[2] !== queueObject.third, "The 'third' (string) property is NOT added the the queue");
+		strictEqual(queue[2], queueObject.fourth, "The 'fourth' method is added the the queue (as queue[2])");
+	});
+
+	test("The object created by 'createJsQueue' has an 'exec' method that executes the queue", function () {
+		var testCount = 0,
+			queueReady = false,
+			one = 0,
+			two = 0,
+			queueObject = {
+				first : function (one, two) {
+					testCount += 1;
+
+					ok(typeof this.ready === "function", "The queue has a 'ready' method that can be executed from within a queue method");
+
+					if (typeof one === "number") {
+						one += 1;
+						two += 1;
+					}
+
+					this.ready(one, two);
+				},
+				second : function (one, two) {
+					testCount += 1;
+
+					if (typeof one === "number") {
+						one += 1;
+						two += 1;
+
+						strictEqual(one, 2, "I'ts possible to send arguments to the queue (test 1)");
+						strictEqual(two, 2, "I'ts possible to send arguments to the queue (test 2)");
+					}
+
+					this.ready(one, two);
+				}
+			},
+			queue = window.createJsQueue(queueObject);
+
+		ok(typeof queue.exec === "function", "There is an 'exec' method");
+
+		queue.exec();
+		strictEqual(testCount, 2, "Both the first and second methods in the queueObject is executed");
+
+		queue.exec(one, two);
+
+		queue.addListener("ready", function () {
+			queueReady = true;
+		});
+		queue.exec();
+		strictEqual(queueReady, true, "A 'ready' event is triggered by the this.ready method in the last queue method");
+	});
+
+}(window));
